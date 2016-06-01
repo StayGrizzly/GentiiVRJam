c63b20e15c3a8a8aa8e3426162fcc9a7374073be
Jake Pierce <johnpierce.gamedev@gmail.com>
2016-06-01 12:09:18 -0400

    Working on bibbit randomization

---
+++ GentiiVRJam/Assets/BibbitsMananger.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BibbitsMananger : MonoBehaviour {
+
+    public GameObject[] m_BibbitHoles;
+    public GameObject[] m_BibbitPrefabs;
+
+    int BibbitHole;
+    int BibbitType;
+
+    bool isBibbitPlaced;
+
+    // Use this for initialization
+    void Start ()
+    {
+        PlaceNewBibbit();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // test = Random.Range(0, m_BibbitPrefabs.Length);
+        // Debug.Log(test);
+
+
+        // Place Bibbit (only if it's not there)
+        // Check if bibbit is still there
+        // if bibbit is gone then confirm buffer
+
+
+	}
+
+    private void PlaceNewBibbit()
+    {
+
+    }
+
+
+
+}
