de389aa0886038cfaa133d321564630a1df5106c
Jake Pierce <johnpierce.gamedev@gmail.com>
2016-06-08 16:39:31 -0400

    Bibbits in cleaning lines now have a customizable spawn rate and amount

--- GentiiVRJam/Assets/BibbitLine.cs
+++ GentiiVRJam/Assets/BibbitLine.cs
@@ -4,43 +4,69 @@ using System.Collections.Generic;
 
 public class BibbitLine : MonoBehaviour {
 
+    // CUSTOMIZABLE VARIABLES
+    public List<GameObject> m_LinePoints;
+    public GameObject m_Bibbit;
+    public int m_MaxBibbits;
+    public float m_BibbitSpeed = 1.0F;
+    public float m_BibbitSpawnRate;
 
     // BIBBIT VARIABLES
-    public GameObject m_Bibbit;
-    List<GameObject> m_SpawnedBibbits = new List<GameObject>();
+    private GameObject m_Spawn;
+    private List<GameObject> m_SpawnedBibbits = new List<GameObject>();
 
-    // LERP VARIABLES
-    public float m_BibbitSpeed = 1.0F;
-    private bool isMovingForward = true;
+    // BIBBIT TIMER
+    private float m_StartTime;
+    private float m_ElapsedTime;
 
-
-    // MOVEMENT POINTS
-    GameObject m_Spawn;
-    public List<GameObject> m_LinePoints;
-
-
-
-    // Use this for initialization
+    // THE BEEF
     void Start ()
     {
         m_Spawn = transform.FindChild("Spawn").gameObject;
-        
+        m_StartTime = Time.time;
 	}
 	
-	// Update is called once per frame
+    // MORE BEEF
 	void Update ()
-    { 
-        if (m_SpawnedBibbits.Count < 1)
+    {
+        m_ElapsedTime = Time.time - m_StartTime;
+        if (m_SpawnedBibbits.Count < m_MaxBibbits)
         {
-            GameObject temp = (GameObject)Instantiate(m_Bibbit, m_Spawn.transform.position, Quaternion.identity);
-            m_SpawnedBibbits.Add(temp);
-            temp.AddComponent<Cleaning_Bibbit>();
-            temp.GetComponent<Cleaning_Bibbit>().SetSpeed(m_BibbitSpeed);
-            temp.GetComponent<Cleaning_Bibbit>().AddToTravelFlags(m_Spawn.transform);
-            SetToPath(temp);
+            if (m_ElapsedTime >= m_BibbitSpawnRate || m_SpawnedBibbits.Count == 0)
+            {
+                SpawnNewBibbit();
+                m_StartTime = Time.time;
+            }
         }
+
+        CheckIfBibbitsDone();
+
 	}
 
+    void SpawnNewBibbit()
+    {
+        GameObject temp = (GameObject)Instantiate(m_Bibbit, m_Spawn.transform.position, Quaternion.identity);
+        m_SpawnedBibbits.Add(temp);
+        temp.AddComponent<Cleaning_Bibbit>();
+        temp.GetComponent<Cleaning_Bibbit>().SetSpeed(m_BibbitSpeed);
+        temp.GetComponent<Cleaning_Bibbit>().AddToTravelFlags(m_Spawn.transform);
+        SetToPath(temp);
+    }
+
+    void CheckIfBibbitsDone()
+    {
+        for (int i = 0; i < m_SpawnedBibbits.Count; ++i)
+        {
+            if (m_SpawnedBibbits[i].GetComponent<Cleaning_Bibbit>().GetIfCompletedRoute() == true)
+            {
+                GameObject temp = m_SpawnedBibbits[i];
+                m_SpawnedBibbits.RemoveAt(i);
+                Destroy(temp);
+            }
+        }
+    }
+
+    // ADDS FLAGS TO INDIVIDUAL SPAWNED BIBBITS
     void SetToPath(GameObject _bibbit)
     {
         for (int i = 0; i < m_LinePoints.Count; ++i)
--- GentiiVRJam/Assets/Cleaning_Bibbit.cs
+++ GentiiVRJam/Assets/Cleaning_Bibbit.cs
@@ -4,119 +4,111 @@ using System.Collections.Generic;
 
 public class Cleaning_Bibbit : MonoBehaviour {
 
+    // CUSTOMIZABLE VARIABLES
+    public float m_MovementSpeed;
 
     // LERPING FLAGS
-    public List<Transform> m_TravelFlags = new List<Transform>();
-    public List<Transform> m_BackFlags = new List<Transform>();
+    private List<Transform> m_TravelFlags = new List<Transform>();
+    private List<Transform> m_BackFlags = new List<Transform>();
 
     // LERPING VARIABLES
-    public float m_MovementSpeed;
     private float m_StartTime;
     private float m_JourneyLength;
     private bool m_IsMovingForward = true;
+    private bool m_IsDoneMoving = false;
+    private bool m_CompletedRoute = false;
+    private int m_ForwardReps = 0;
+    private int m_BackwardReps = 0;
+    
 
+    // GETTERS AND SETTERS
+    public bool GetIfCompletedRoute() { return m_CompletedRoute; }
 
-    public Transform DEBUG_TEST_LOC_S;
-    public Transform DEBUG_TEST_LOC_M;
-    public Transform DEBUG_TEST_LOC_E;
-    bool DEBUG_DONE_MOVING = false;
-    int DEBUG_FORWARD_REPS = 0;
-    int DEBUG_BACKWARD_REPS = 0;
+    public void SetSpeed(float _speed) { m_MovementSpeed = _speed; }
+    public void AddToTravelFlags(Transform _transform) { m_TravelFlags.Add(_transform); }
 
-    public void SetSpeed(float _speed)
-    {
-        m_MovementSpeed = _speed;
-    }
-
-
-    public void AddToTravelFlags(Transform _transform)
-    {
-        m_TravelFlags.Add(_transform);
-    }
-
-	// Use this for initialization
+    // THE BEEF
 	void Start ()
     {
         m_StartTime = Time.time;
 	}
 	
-	// Update is called once per frame
+    // MORE BEEF
 	void Update ()
     {
         if (m_TravelFlags.Count > 0 || m_BackFlags.Count > 0)
         {
-            Debug.Log("There are flags!");
-            if (m_IsMovingForward)
-            {
+            //Debug.Log("There are flags!");
 
+            if (m_IsMovingForward)
                 MoveForward();
-            }
 
             else
-            {
                 MoveBackward();
-            }
         }
+
+        if (m_ForwardReps == m_BackwardReps && m_ForwardReps > 0)
+            m_CompletedRoute = true;
     }
 
-
+    // MOVE BIBBIT FORWARD ON PATH
     void MoveForward()
     {
-        Debug.Log("Moving Forward!");
+        //Debug.Log("Moving Forward!");
 
         Lerp(m_TravelFlags[0], m_TravelFlags[1]);
 
-        if (DEBUG_DONE_MOVING)
+        if (m_IsDoneMoving)
         {
             m_BackFlags.Add(m_TravelFlags[0]);
             m_TravelFlags.RemoveAt(0);
-            DEBUG_DONE_MOVING = false;
+            m_IsDoneMoving = false;
         }
 
-        Debug.Log("Travel Flag Count: " + m_TravelFlags.Count);
+        //Debug.Log("Travel Flag Count: " + m_TravelFlags.Count);
         if (m_TravelFlags.Count == 1)
             {
-                Debug.Log("Done Moving Forward!");
+                //Debug.Log("Done Moving Forward!");
                 m_BackFlags.Add(m_TravelFlags[0]);
                 m_TravelFlags.RemoveAt(0);
 
                 m_IsMovingForward = false;
-                DEBUG_DONE_MOVING = false;
-                ++DEBUG_FORWARD_REPS;
-                Debug.Log("FORWARD REPS: " + DEBUG_FORWARD_REPS);
+                m_IsDoneMoving = false;
+                ++m_ForwardReps;
+                //Debug.Log("FORWARD REPS: " + m_ForwardReps);
                 m_BackFlags.Reverse();
             }
     }
 
+    // MOVE BIBBIT BACKWARD ON PATH
     void MoveBackward()
     {
-        Debug.Log("Moving Backward!");
+        //Debug.Log("Moving Backward!");
 
         Lerp(m_BackFlags[0], m_BackFlags[1]);
 
-        if (DEBUG_DONE_MOVING)
+        if (m_IsDoneMoving)
         {
             m_TravelFlags.Add(m_BackFlags[0]);
             m_BackFlags.RemoveAt(0);
-            DEBUG_DONE_MOVING = false;
+            m_IsDoneMoving = false;
         }
 
         if (m_BackFlags.Count == 1)
         {
-            Debug.Log("Done Moving Backward!");
+            //Debug.Log("Done Moving Backward!");
             m_TravelFlags.Add(m_BackFlags[0]);
             m_BackFlags.RemoveAt(0);
 
             m_IsMovingForward = true;
-            DEBUG_DONE_MOVING = false;
-            ++DEBUG_BACKWARD_REPS;
-            Debug.Log("BACKWARD REPS: " + DEBUG_BACKWARD_REPS);
+            m_IsDoneMoving = false;
+            ++m_BackwardReps;
+            //Debug.Log("BACKWARD REPS: " + m_BackwardReps);
             m_TravelFlags.Reverse();
         }
     }
 
-
-
+    // LERP FUNCTION (USES TWO FLAG PARAMETERS)
     void Lerp(Transform _start, Transform _end)
     {
         gameObject.transform.position = _start.position;
@@ -135,7 +127,7 @@ public class Cleaning_Bibbit : MonoBehaviour {
         {
             // isDoneWithMove = true;
             m_StartTime = Time.time;
-            DEBUG_DONE_MOVING = true;
+            m_IsDoneMoving = true;
         }
     }
 
