33e20c056e172c5d570a66b28210c994f80e3af7
Jake Pierce <johnpierce.gamedev@gmail.com>
2016-06-13 14:36:09 -0400

    Fixed the fucking scene for the 100th time

--- GentiiVRJam/Assets/SoilEvent.cs
+++ GentiiVRJam/Assets/SoilEvent.cs
@@ -4,16 +4,29 @@ using System.Collections;
 public class SoilEvent : MonoBehaviour {
 
     public GameObject m_VineWand;
+    public GameObject m_ParticleFX;
+    GameObject wand;
+    private bool m_HasAnimPlayed = false;
 
     void OnTriggerEnter(Collider col)
     {
         if(col.gameObject.tag == "Seed")
         {
-            GameObject wand = (GameObject)Instantiate(m_VineWand, transform.position, Quaternion.identity);
-            wand.transform.GetChild(0).GetComponent<Animation>().Play();
             Destroy(col.gameObject);
+            wand = (GameObject)Instantiate(m_VineWand, new Vector3(transform.position.x, transform.position.y+.25f, transform.position.z), Quaternion.identity);
+            GameObject particle = (GameObject)Instantiate(m_ParticleFX, new Vector3(transform.position.x, transform.position.y + .5f, transform.position.z), Quaternion.Euler(-90, 0, 0));
         }
     }
-
+    /*
+    void Update()
+    {
+        if (wand.GetComponent<Animation>().isPlaying == false && m_HasAnimPlayed == true)
+        {
+            GameObject finalwand = (GameObject)Instantiate(m_VineWand, wand.transform.position, Quaternion.identity);
+            Destroy(wand);
+            Destroy(gameObject);
+        }
+    }
+    */
 
 }
